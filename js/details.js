949acd511de48e4454d863b49bb5cba6837fd0b5
iliasmerk <66678327+iliasmerk@users.noreply.github.com>
2020-06-29 12:05:38 +0300

    Update details.js

--- js/details.js
+++ js/details.js
@@ -1,13 +1,15 @@
 
  
   
-function myFunction() { 
-    modal.style.display = "block";
-}
+
      
 var modal = document.getElementById("myModal");  
 var span = document.getElementsByClassName("close")[0];
-     
+
+function myFunction() { 
+    modal.style.display = "block";
+}
+
 span.onclick = function() {
   modal.style.display = "none";
 } 
