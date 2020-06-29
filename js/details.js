02cdef4ea3a4a16cecea59af71aeb17c38ae2710
iliasmerk <66678327+iliasmerk@users.noreply.github.com>
2020-06-29 11:46:35 +0300

    Create details.js

---
+++ js/details.js
@@ -0,0 +1,52 @@
+
+<script>
+  
+function myFunction() { 
+    modal.style.display = "block";
+}
+     
+var modal = document.getElementById("myModal");  
+var span = document.getElementsByClassName("close")[0];
+     
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+    }
+
+    function showhideprev() {
+        if (document.getElementById('backgroundgreyprev').style.display == 'none') {
+
+            $("#toggleleftarrayimgprev").addClass("rotate");
+        } else {
+
+            $("#toggleleftarrayimgprev").removeClass("rotate");
+        }
+        $('#backgroundgreyprev').toggle('hide');
+    }
+    
+    function showhide() {
+        if (document.getElementById('toggleleftarray').style.display == 'none') {
+
+            $("#toggleleftarrayimg").addClass("rotate");
+        } else {
+
+            $("#toggleleftarrayimg").removeClass("rotate");
+        } 
+        $('.toggleleftarray').toggle('hide');
+    }
+
+    function showrighthide() { 
+        if (document.getElementById('hiderightpanel').style.display == 'none') {
+     
+            $("#rightimagesarrow").addClass("rotate");
+        } else { 
+            $("#rightimagesarrow").removeClass("rotate");
+        }
+        
+        $('.hiderightpanel').toggle('hide');
+       }
+</script>
